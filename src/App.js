f11bfa641c38f46e2beb0fe71f97b041e6bd1d8d
Hyowon Park <sayyoursong@gmail.com>
2020-01-23 23:20:38 +0900

    learn component life cycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+  componentDidMount() {
+    console.log("component did rendered.");
+  }
+  componentDidUpdate() {
+    console.log("updated.")
+  }
   state = {
     number : 0
   }
@@ -14,6 +24,7 @@ class App extends React.Component {
 
   render() {
     return <>
+      {console.log("rent")}
       <h1>hello world! {this.state.number}</h1>
       <button onClick={this.add}>plus</button>
       <button onClick={this.minus}>minus</button>
