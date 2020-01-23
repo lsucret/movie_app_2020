773d642372cfa2052055806500f0bd1ef20951b1
Hyowon Park <sayyoursong@gmail.com>
2020-01-23 23:05:15 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    number : 0
+  }
+  add = () => {
+    this.setState(current => ({number: current.number + 1}))
+  }
+
+  minus = () => {
+    this.setState(current => ({number: current.number - 1}))
+  }
+
+  render() {
+    return <>
+      <h1>hello world! {this.state.number}</h1>
+      <button onClick={this.add}>plus</button>
+      <button onClick={this.minus}>minus</button>
+    </>
+  }
 }
-
 export default App;
