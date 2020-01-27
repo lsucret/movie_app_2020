02d6357154e46dd7691f4ccc6bd4803cc10f1cbd
Hyowon Park <sayyoursong@gmail.com>
2020-01-27 18:21:42 +0900

    Cutting the Summary

--- src/Movie.js
+++ src/Movie.js
@@ -4,17 +4,19 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return ( 
-        <div className="movies__movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />  
             <div className="movie_data">
-                <h3 className="movie__title" style={{backgroundColor:"red"}}>{title}</h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">
+                <ul className="movie__genres">
                     {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre">{genre}</li>
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
                     ))}
                 </ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0,180)}...</p>
             </div>  
         </div>
     )
